b5a77ccc026405c60f74ae1060891952b11376f4
AfonsoCml <116306030+AfonsoCml@users.noreply.github.com>
2025-12-05 17:40:18 +0000

    Models added

---
+++ SomiodMiddleware/Models/ContentInstance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SomiodMiddleware.Models
+{
+    public class ContentInstance
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public  string contentType { get; set; }
+        public string content { get; set; }
+        public DateTime Created_at { get; set; }
+    }
+}
---
+++ SomiodMiddleware/Models/Subscription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SomiodMiddleware.Models
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int evt { get; set; }
+        public string content { get; set; }
+        public DateTime Created_at { get; set; }
+    }
+}
